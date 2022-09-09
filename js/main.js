2e0d747d935940b6cd50904e34a50eced33a01a1
Abdullah Al Ahad Bhuiyan <102456801+anondobhaii@users.noreply.github.com>
2022-09-09 14:34:41 +0600

    ;-;

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const email_bar = document.querySelector("#email_bar");
 const input = document.querySelector("#email");
 const submit = document.querySelector("#submit");
+const error = document.querySelector("#error");
+const error__msg = document.querySelector("#error__msg");
 setInterval(() => {
   console.log(input.value);
 }, 1000);
@@ -11,14 +13,14 @@ submit.addEventListener("click", () => {
   let email = input.value;
   
   if(!regex.test(email)){
-    email_bar.style.borderColor = "red";
-    email_bar.style.borderColor = "red";
-    email_bar.style.borderColor = "red";
-    email_bar.style.borderColor = "red";
-
+    email_bar.style.borderColor = "hsl(0, 93%, 68%)";
+    error.classList.remove("d-none");
+    error__msg.classList.remove("d-none");
   }
   else{
     email_bar.style.borderColor = "hsl(0, 36%, 70%, 0.5)";
+    error.classList.add("d-none");
+    error__msg.classList.add("d-none");
   }
 });
 
